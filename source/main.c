c9fbf9ecf1dbacaf9a3f5bf46f115d0784241b7f
Krzysztof Brzozowski <krzysztof_brzozowski@icloud.com>
2023-10-05 09:36:05 +0200

    Trying to add wifi scan into project - not building yet

--- source/main.c
+++ source/main.c
@@ -2,6 +2,7 @@
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
 #include "hardware/uart.h"
+#include "scan_wifi.h"
 
 
 #define UART_ID uart0
@@ -13,35 +14,35 @@
 #define UART_RX_PIN 1
 
 int main() {
+    /* Start WiFi init */
     stdio_init_all();
 
-    if (cyw43_arch_init())
+    if (cyw43_arch_init()) {
+        printf("Failed to initialise WiFi\n");
         return -1;
-
-        // Set up our UART with the required speed.
+    }
+    
+    cyw43_arch_enable_sta_mode();
+    /* End WiFi init */
+    /* Start UART init */
     uart_init(UART_ID, BAUD_RATE);
 
-    // Set the TX and RX pins by using the function select on the GPIO
-    // Set datasheet for more information on function select
     gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
     gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+    /* End UART init */
 
-    // Use some the various UART functions to send out data
-    // In a default system, printf will also output via the default UART
 
-    // Send out a character without any conversions
-    uart_putc_raw(UART_ID, 'A');
-
-    // Send out a character but do CR/LF conversions
-    uart_putc(UART_ID, 'B');
-
-    // Send out a string, with CR/LF conversions
-    uart_puts(UART_ID, " Hello, UART!\n");
 
     while (true) {
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
         sleep_ms(1000);
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
         sleep_ms(1000);
+        
+        do_wifi_scan();
+
+        uart_putc_raw(UART_ID, 'A');
+        uart_putc(UART_ID, 'B');
+        uart_puts(UART_ID, " Hello, UART!\n");
     }
 }
