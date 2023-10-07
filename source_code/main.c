17573665b81c43bd6ab07e7459c30587c3c4ebc1
Krzysztof Brzozowski <krzysztof_brzozowski@icloud.com>
2023-10-07 10:20:46 +0200

    Switched wifi lib to polling, scanning wifi working

--- source_code/main.c
+++ source_code/main.c
@@ -27,7 +27,7 @@ int main() {
     while (true) {
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
         sleep_ms(1000);
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
         sleep_ms(1000);
         
         wifi_scan();
